e5ca34a0110dc415cde291cb8d919ce65ea71f92
Denis <Orlenko42@gmail.com>
2021-09-29 17:19:43 +0300

    Initial Commit

---
+++ IviCloneApp/IviCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IviCloneAppApp.swift
+//  IviCloneApp
+//
+//  Created by Denis on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct IviCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
